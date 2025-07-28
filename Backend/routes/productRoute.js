f200c843c9d22e323314b1ad91e1be10ccec0836
Ifrahzainab23 <freeifrah35@gmail.com>
2025-07-28 15:56:48 +0500

    Add files via upload

---
+++ Backend/routes/productRoute.js
@@ -0,0 +1,30 @@
+import express from "express";
+import productController from "../controllers/productController.js";
+import upload from "../middleware/multer.js";
+import adminAuth from "../middleware/adminAuth.js";
+
+const productRouter = express.Router();
+
+// Admin: Add Product
+productRouter.post(
+  "/add",
+  adminAuth,
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),
+  productController.addProduct
+);
+
+// Admin: Remove Product
+productRouter.post("/remove", adminAuth, productController.removeProduct);
+
+// Public: Get Single Product
+productRouter.post("/single", productController.singleProduct);
+
+// Public: List Products
+productRouter.get("/list", productController.listProducts);
+
+export default productRouter;
